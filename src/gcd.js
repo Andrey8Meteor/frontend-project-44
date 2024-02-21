9738eb9e381a1a9f5e2d80d7678fba1292e058f0
Andrey <andre_raz@list.ru>
2024-02-21 22:25:47 +0300

    ajdh

--- src/gcd.js
+++ src/gcd.js
@@ -3,18 +3,18 @@ import runGame from './index.js';
 
 const gameDescription5 = 'Find the greatest common divisor of given numbers.';
 
-const getGsd = (firstValue, secondValue) => {
+const getGcd = (firstValue, secondValue) => {
   if (!secondValue) {
     return firstValue;
   }
-  return getGsd(secondValue, firstValue % secondValue);
+  return getGcd(secondValue, firstValue % secondValue);
 };
 
 const getGame = () => {
   const num1 = getRandomNum(1, 100);
   const num2 = getRandomNum(1, 100);
   const question1 = `${num1} ${num2}`;
-  const correctAns = getGsd(num1, num2).toString;
+  const correctAns = getGcd(num1, num2).toString();
 
   return [question1, correctAns];
 };
