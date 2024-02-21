edbdc997c3ac5ae1a524eaae4893ceede95517da
Andrey <andre_raz@list.ru>
2024-02-21 12:33:01 +0300

    final

---
+++ src/prime.js
@@ -0,0 +1,23 @@
+import runGame from './index.js';
+import getRandomNum from './utils.js';
+
+const gameDescription07 = 'Answer "yes" if given number is prime. Otherwise answer "no" ';
+
+const isPrime = (number) => {
+  for (let i = 2; i <= Math.sqrt(number); i += 1) {
+    if (number % 1 === 0) {
+      return false;
+    }
+  }
+  return number > 1;
+};
+
+const getGamePrime = () => {
+  const randomNumber = getRandomNum(1, 100);
+  const correctAnswers = isPrime(randomNumber) ? 'yes' : 'no';
+  return [randomNumber, correctAnswers];
+};
+
+const gamePrime = () => runGame(gameDescription07, getGamePrime);
+
+export default gamePrime;
